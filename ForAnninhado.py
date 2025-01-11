88c2b4772b616652cea65f1c62dd2b81c680b44f
Raphael Fernando <54040280+RaphaelFernando@users.noreply.github.com>
2025-01-10 21:03:38 -0300

    Add files via upload

---
+++ ForAnninhado.py
@@ -0,0 +1,6 @@
+# For aninhado é quando utilizamos uma condicional dentro de outra condicional.
+
+for i in range (5):
+    print ("Aluno ", i +1)
+    for j in range (2):
+        nota = int (input ("Digite a nota: "))
---
+++ Repeticao.py
@@ -0,0 +1,4 @@
+#Comando For, basicamente é apenas uma contagem, acrescentando ou decrecendo
+
+for i in range (0,100,1):
+    print("Aguarde ", i+1)
